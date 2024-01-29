9e0f6557b2630ab5d2c83959bf2ac31e7b159a04
ChShreya11 <98463362+ChShreya11@users.noreply.github.com>
2024-01-29 16:55:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let a =document.getElementById("tryit");
-console.log('running');
-a.addEventListener("click", function() {
-    // Redirect to the dashboard page
+// let a =document.getElementById("tryit");
+// console.log('running');
+// a.addEventListener("click", function() {
+//     // Redirect to the dashboard page
 
-    window.location.href = "dashboard.html";
-});
+//     window.location.href = "dashboard.html";
+// });
